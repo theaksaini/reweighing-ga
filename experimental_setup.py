92249483c12bd3c05c918588228f96ae65e097e2
Anil Kumar Saini <anil1iitr@gmail.com>
2025-08-25 15:57:54 -0700

    added rus support

--- experimental_setup.py
+++ experimental_setup.py
@@ -6,12 +6,13 @@ import pickle
 from functools import partial
 import traceback
 from sklearn.model_selection import train_test_split
+from imblearn.under_sampling import RandomUnderSampler
 
 import utils
 # modify for merged ga/nsga
 from ga import GA
 
-def loop_with_equal_evals2(ml_models, experiments, task_id_lists, base_save_folder, data_dir, num_runs, objective_functions, objective_functions_weights , ga_params):
+def compare_reweighting_methods(ml_models, experiments, task_id_lists, base_save_folder, data_dir, num_runs, objective_functions, objective_functions_weights , ga_params):
     for m, ml in enumerate(ml_models):
         for t, taskid in enumerate(task_id_lists):
             for r in range(num_runs):
@@ -39,6 +40,11 @@ def loop_with_equal_evals2(ml_models, experiments, task_id_lists, base_save_fold
                     # Split the training set into training and validation sets
                     X_train, X_val, y_train, y_val = train_test_split(X_train_val, y_train_val, test_size=0.1765, stratify=y_train_val, random_state=r)
 
+                    if taskid.startswith('pmad_rus'):
+                        #Random undersampling because of extreme class imbalance
+                        rus = RandomUnderSampler(sampling_strategy = "auto", random_state=r)
+                        X_train, y_train = rus.fit_resample(X_train, y_train)
+                    
                     print("starting ml")
                                     
                     try:  
@@ -86,13 +92,12 @@ def loop_with_equal_evals2(ml_models, experiments, task_id_lists, base_save_fold
                             with open(f"{save_folder}/scores.pkl", "wb") as f:
                                 pickle.dump(scores, f)
 
-                            if exp == 'Deterministic Weights':
-                                return
+                            return
                         
-                        else:
+                        elif exp == 'Evolved Weights':
                             scores = pd.DataFrame(columns = ['taskid','exp_name','seed', 'run', *objective_functions, *['train_'+k for k in objective_functions]])
                             ga_func = partial(utils.fitness_func_holdout, model = ml(random_state=super_seed), X_train=X_train, y_train=y_train, X_val =X_val, y_val=y_val, 
-                                              sens_features=sens_features, objective_functions=objective_functions, objective_functions_weights=objective_functions_weights)
+                                              sens_features=sens_features, objective_fuctions=objective_functions, objective_functions_weights=objective_functions_weights)
                             ga_func.__name__ = 'ga_func'
                             
                             # changing to merged
@@ -134,6 +139,10 @@ def loop_with_equal_evals2(ml_models, experiments, task_id_lists, base_save_fold
                                 pickle.dump(scores, f)
 
                             return
+                        
+                        else:
+                            raise ValueError("Invalid experiment name")
+                        
                     except Exception as e:
                         trace =  traceback.format_exc()
                         pipeline_failure_dict = {"taskid": taskid, "exp_name": exp,  "error": str(e), "trace": trace}
@@ -195,7 +204,7 @@ def loop_with_equal_evals3(ml_models, experiments, task_id_lists, base_save_fold
                                 X_train=X_train, y_train=y_train, 
                                 X_val=X_val, y_val=y_val, 
                                 sens_features=sens_features, 
-                                objective_functions=['accuracy', 'subgroup_fnr'], 
+                                objective_fuctions=['accuracy', 'subgroup_fnr'], 
                                 objective_functions_weights=[1, -1]
                             )
                             use_nsga = False
@@ -278,7 +287,7 @@ def loop_with_equal_evals3(ml_models, experiments, task_id_lists, base_save_fold
         
     print("all finished")
 
-def loop_with_equal_evals4(ml_models, experiments, task_id_lists, base_save_folder, data_dir, num_runs, objective_functions, objective_functions_weights, ga_params):
+def compare_partial_and_full_dataset_evolved_weights(ml_models, experiments, task_id_lists, base_save_folder, data_dir, num_runs, objective_functions, objective_functions_weights, prop, ga_params):
     for m, ml in enumerate(ml_models):
         for t, taskid in enumerate(task_id_lists):
             for r in range(num_runs):
@@ -322,8 +331,8 @@ def loop_with_equal_evals4(ml_models, experiments, task_id_lists, base_save_fold
                         # Logic for Transfer_Weights_Holdout
                         if exp == "Transfer_Weights_Holdout":
                             print("Applying Transfer Weights Method...")
-                            # Sample a% of training data for weight evolution. Currently, 10%
-                            a = 0.1
+                            # Sample a% of training data for weight evolution. Currently, 0.1 and 0.5
+                            a = prop
                             X_sample, _, y_sample, _ = train_test_split(
                                 X_train, y_train, test_size=(1 - a), stratify=y_train, random_state=r
                             )
@@ -337,7 +346,7 @@ def loop_with_equal_evals4(ml_models, experiments, task_id_lists, base_save_fold
                                 X_val=X_val,
                                 y_val=y_val,
                                 sens_features=sens_features,
-                                objective_functions=objective_functions,
+                                objective_fuctions=objective_functions,
                                 objective_functions_weights=objective_functions_weights
                             )
                             ga_func.__name__ = 'ga_func'
@@ -397,7 +406,7 @@ def loop_with_equal_evals4(ml_models, experiments, task_id_lists, base_save_fold
                                 X_val=X_val,
                                 y_val=y_val,
                                 sens_features=sens_features,
-                                objective_functions=objective_functions,
+                                objective_fuctions=objective_functions,
                                 objective_functions_weights=objective_functions_weights
                             )
                             ga_func.__name__ = 'ga_func'
@@ -417,22 +426,21 @@ def loop_with_equal_evals4(ml_models, experiments, task_id_lists, base_save_fold
                                 est = ml(random_state=super_seed)
                                 weights = utils.partial_to_full_sample_weight(
                                     ga.evaluated_individuals.loc[j, 'individual'],
-                                    X_train_val,
-                                    y_train_val,
+                                    X_train,
+                                    y_train,
                                     sens_features
                                 )
-                                est.fit(X_train_val, y_train_val, weights)
+                                est.fit(X_train, y_train, weights)
 
                                 # Evaluate scores
                                 train_score = utils.evaluate_objective_functions(
-                                    est, X_train_val, y_train_val, objective_functions, sens_features
+                                    est, X_train, y_train, objective_functions, sens_features
                                 )
                                 test_score = utils.evaluate_objective_functions(
                                     est, X_test, y_test, objective_functions, sens_features
                                 )
 
                                 # Record scores
-                                this_score = {}
                                 this_score = {f"train_{k}": v for k, v in train_score.items()}
                                 this_score.update(test_score)
                                 this_score["taskid"] = taskid
@@ -445,8 +453,6 @@ def loop_with_equal_evals4(ml_models, experiments, task_id_lists, base_save_fold
                         # Save scores
                         with open(f"{save_folder}/scores.pkl", "wb") as f:
                             pickle.dump(scores, f)
-                        
-                        return
 
                     except Exception as e:
                         # Handle exceptions and log failures
@@ -464,8 +470,6 @@ def loop_with_equal_evals4(ml_models, experiments, task_id_lists, base_save_fold
 
                         with open(f"{save_folder}/failed.pkl", "wb") as f:
                             pickle.dump(pipeline_failure_dict, f)
-                        
-                        return
 
     print("all finished")
 
--- main.py
+++ main.py
@@ -21,7 +21,7 @@ import experimental_setup as experimental_setup
 
 def main():
     ml_models = [RandomForestClassifier, LogisticRegression, XGBClassifier]
-    datasets_binary = ['heart_disease', 'student_math', 'student_por', 'creditg', 'titanic', 'us_crime', 'compas_violent', 'nlsy', 'compas', 'law_school', 'pmad_phq', 'pmad_epds']
+    datasets_binary = ['heart_disease', 'student_math', 'student_por', 'creditg', 'titanic', 'us_crime', 'compas_violent', 'nlsy', 'compas', 'law_school', 'pmad_phq', 'pmad_epds', 'pmad_rus_phq', 'pmad_rus_epds']
     experiments1 = ['Equal Weights', 'Deterministic Weights', 'Evolved Weights']
     experiments2 = ['Transfer_Weights_Holdout', 'Evolved_Weights_Holdout', 'Evolved_Weights_Lexidate']
 
@@ -32,8 +32,8 @@ def main():
     data_dir = './Datasets'
      
     #local
-    
-    experimental_setup.loop_with_equal_evals2(ml_models= ml_models[0:1],
+
+    experimental_setup.compare_reweighting_methods(ml_models= ml_models[0:1],
                                 experiments=experiments1,
                                 task_id_lists=datasets_binary[0:3],
                                 base_save_folder='results',
@@ -45,7 +45,7 @@ def main():
                                 )
     
     #hpc
-    #experimental_setup.loop_with_equal_evals2(ml_models= ml_models[0:1],
+    #experimental_setup.compare_reweighting_methods(ml_models= ml_models[0:1],
     #                            experiments=experiments1,
     #                            task_id_lists=datasets_binary,
     #                            base_save_folder='results',
@@ -57,7 +57,7 @@ def main():
     #                            )
 
     # Test new loop for Transfer_Weights_Holdout
-    #experimental_setup.loop_with_equal_evals4(
+    #experimental_setup.compare_partial_and_full_dataset_evolved_weights(
     #    ml_models=ml_models[0:1],  # Test with the first model
     #    experiments = experiments2[0:2],  # Test with Transfer_Weights_Holdout and Evolved_Weights_Holdout
     #    task_id_lists=datasets_binary[3:4],  # Test with the 4th dataset
